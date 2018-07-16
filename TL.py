af4eed037b5e6e6e54c46ead474a20d285906196
apoorvb <apoorvbhardwaj88@gmail.com>
2018-07-17 04:20:05 +0530

    Update TL.py

--- TL.py
+++ TL.py
@@ -45,12 +45,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('/home/apocobis/Downloads/Dataset/cat&dog/training_set',
+training_set = train_datagen.flow_from_directory('..input/Dataset/cat&dog/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  classes = ['dogs', 'cats'])
 
-test_set = test_datagen.flow_from_directory('/home/apocobis/Downloads/Dataset/cat&dog/test_set',
+test_set = test_datagen.flow_from_directory('..input/Dataset/cat&dog/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             classes = ['dogs', 'cats'])
@@ -92,12 +92,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('/home/apocobis/Downloads/Dataset/cat&dog/training_set',
+training_set = train_datagen.flow_from_directory('..input/Dataset/cat&dog/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('/home/apocobis/Downloads/Dataset/cat&dog/test_set',
+test_set = test_datagen.flow_from_directory('..input/Dataset/cat&dog/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
